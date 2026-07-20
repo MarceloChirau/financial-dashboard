8e46f3d6013be4fcafe69fb9f39f90c68f33a799
marcelo <marcelochirau@gmail.com>
2026-07-20 10:21:01 +0200

    improve error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,6 +29,7 @@ export async function createInvoice(formData:FormData){
 // console.log(rawFormData)
 
 //we refactor to validate types with zod
+
 const{customerId,amount,status}=CreateInvoice.parse({
     customerId:formData.get('customerId'),
     amount:formData.get('amount'),
@@ -38,21 +39,36 @@ const{customerId,amount,status}=CreateInvoice.parse({
 //convert amounts in cents`:
 const amountInCents=amount*100;
 const date=new Date().toISOString().split('T')[0];
+try{
+    
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+  
+}
+catch(error){
+console.error(error)
+return{
+    message:'Database Error: failed to Create Onvoice.'
+}
 
-await sql`
-INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`
-//Since you're updating the data displayed in the invoices route,
-//  you want to clear this cache and trigger a new request to the server.
-//  You can do this with the revalidatePath function from Next.js:
-//Once the database has been updated, the /dashboard/invoices path will be revalidated,
-//  and fresh data will be fetched from the server.
-revalidatePath('/dashboard/invoices');
+}
 
-//At this point, you also want to redirect the user back to the /dashboard/invoices page.
-//  You can do this with the redirect function from Next.js:
-redirect('/dashboard/invoices');
+  //Since you're updating the data displayed in the invoices route,
+    //  you want to clear this cache and trigger a new request to the server.
+    //  You can do this with the revalidatePath function from Next.js:
+    //Once the database has been updated, the /dashboard/invoices path will be revalidated,
+    //  and fresh data will be fetched from the server.
+    revalidatePath('/dashboard/invoices');
+    
+    //At this point, you also want to redirect the user back to the /dashboard/invoices page.
+    //  You can do this with the redirect function from Next.js:
+    redirect('/dashboard/invoices');
+
+
+
+
 }
 
 
@@ -69,18 +85,35 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
+
+  try{
+
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+     
+  }
+ catch(error){
+    console.error(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+ }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+
+try{
+
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+}catch(error){
+    console.error(error)
+
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+}
+
+
     revalidatePath('/dashboard/invoices');
   }
